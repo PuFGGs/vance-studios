c64b3e72d83167162efd45fcae88d2336d88f794
PuFGGs <dorukakin35@hotmail.com>
2022-04-04 22:06:54 +0100

    Update OurProjects.js

--- src/components/OurProjects/OurProjects.js
+++ src/components/OurProjects/OurProjects.js
@@ -13,7 +13,7 @@ class OurProjects extends React.Component {
                 <h1 className='noselect'>{this.props.titleText}</h1>
                 <Content image={jt_logo} title='JEWEL THIEF' type='Isometric Robbery Game' borderRadius='15px'/>
                 <Content image={er_logo} title='EXPLOIT RUSH' type='Hyper Casual Game' borderRadius='15px'/>
-                <Content image={mxc_logo} title='MEXICAN CASINO' type='Blackjack Table Game' borderRadius='15px'/>
+                <Content image={mxc_logo} title='MX CASINO' type='Blackjack Table Game' borderRadius='15px'/>
                 <Content image={hg_logo} title='HACKERGAME (WIP)' type='Hacking Simulation Game' progress='25' borderRadius='15px'/>
             </div>
         );
